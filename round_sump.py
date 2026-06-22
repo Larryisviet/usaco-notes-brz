f24ffa9b57350edd05067343efb2dd1b98f127e6
larryfromphx <larrytranforbusiness@gmail.com>
2026-06-22 16:59:02 -0700

    Create round_sump.py

---
+++ round_sump.py
@@ -0,0 +1,7 @@
+def round10(num):
+  if (num%10) <= 4:
+    return num - abs((num%10))
+  else:
+    return num + abs(10-(num%10))
+def round_sum(a, b, c):
+  return round10(a) + round10(b) + round10(c)
