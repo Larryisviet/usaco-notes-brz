1423e033b19e6329be7ae87e27959756a70494b6
larryfromphx <larrytranforbusiness@gmail.com>
2026-06-23 11:31:20 -0700

    Update USACO 2015 December Contest, Bronze Problem 2. Speeding Ticket.py

--- USACO 2015 December Contest, Bronze Problem 2. Speeding Ticket.py
+++ USACO 2015 December Contest, Bronze Problem 2. Speeding Ticket.py
@@ -1,21 +1,28 @@
-seg = input().split()
+fin = open("speeding.in", "r")
+seg = fin.readline().split()
 n = int(seg[0])
 m = int(seg[1])
 limit = []
 for a in range(n):
-    road = input().split()
-    length = int(line[0])
-    speed_limit = int(line[1])
+    road = fin.readline().split()
+    length = int(road[0])
+    speed_limit = int(road[1])
     for b in range(length):
         limit.append(speed_limit)
 speed = []
 for c in range(m):
-  road = input().split()
-  length = int(line[0])
-  s = int(line[1])
-  for b in range(length):
+    road = fin.readline().split()
+    length = int(road[0])
+    s = int(road[1])
+    for b in range(length):
         speed.append(s)
 answer = 0
 for i in range(100):
     answer = max(answer, speed[i] - limit[i])
-return answer
+
+if answer < 0:
+    answer = 0
+
+fout = open("speeding.out", "w")
+fout.write(str(answer) + "\n")
+fout.close()
